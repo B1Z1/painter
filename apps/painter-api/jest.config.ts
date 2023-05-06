4d1b7b713aa406babf0c1764f82cb6f383f44c36
Ilya <iluhamishkin@gmail.com>
2023-05-06 12:07:12 +0200

    Add api application

---
+++ apps/painter-api/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'painter-api',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/painter-api',
+};
